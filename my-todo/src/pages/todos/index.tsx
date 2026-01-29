c5e631e74a1c3c0433c5b6d52a7a4bfb2316ee0b
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2026-01-29 10:03:52 +0700

    fix: refactor code

--- my-todo/src/pages/todos/index.tsx
+++ my-todo/src/pages/todos/index.tsx
@@ -30,80 +30,81 @@ export default function Todos() {
         setTodoCategory("")
     }
 
-    console.log("todos : ", todos)
-
     return (
-        <div className="max-w-xl mx-auto p-6 space-y-4">
-            <h1 className="text-2xl font-bold">My Todo</h1>
+        <div className="w-screen h-svh flex justify-center items-center">
+            <div className="max-w-xl h-full mx-auto p-6 space-y-10">
+                <h1 className="text-2xl text-center font-bold">My Todo</h1>
 
-            {/* FORM */}
-            <div className="space-y-2">
-                <input
-                    type="text"
-                    className="border p-2 w-full"
-                    placeholder="Title ..."
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                <input
-                    type="text"
-                    className="border p-2 w-full"
-                    placeholder="Description ..."
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                />
-                <input
-                    type="text"
-                    className="border p-2 w-full"
-                    placeholder="Category ..."
-                    value={todoCategory}
-                    onChange={(e) => setTodoCategory(e.target.value)}
-                />
-                <Button onClick={handleAdd}>Add Todo</Button>
-            </div>
-
-            {/* SEARCH & FILTER */}
-            <div className="flex gap-2">
-                <input
-                    className="border p-2 flex-1"
-                    type="text"
-                    placeholder="Search ..."
-                    value={category}
-                    onChange={(e) => setSearch(e.target.value)}
-                />
-                <select
-                    className="border p-2"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                >
-                    <option value="all">All</option>
-                    <option value="work">Work</option>
-                    <option value="personal">Personal</option>
-                </select>
-                <select
-                    className="border p-2"
-                    value={sort}
-                    onChange={(e) => setSort(e.target.value as "asc" | "desc")}
-                >
-                    <option value="desc">Newest</option>
-                    <option value="asc">Oldes</option>
-                </select>
-            </div>
-
-            {/* LIST */}
-            <div className="space-y-3">
-                {todos?.map((todo: Todo) => (
-                    <TodoCard
-                        key={todo.id}
-                        todo={todo}
-                        onToggle={() => updateTodo(
-                            todo.id,
-                            { isCompleted: !todo.isCompleted }
-                        )}
-                        onDelete={() => deleteTodo(todo.id)}
+                {/* FORM */}
+                <div className="space-y-2">
+                    <input
+                        type="text"
+                        className="border p-2 w-full"
+                        placeholder="Title ..."
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
                     />
-                ))}
+                    <input
+                        type="text"
+                        className="border p-2 w-full"
+                        placeholder="Description ..."
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        className="border p-2 w-full"
+                        placeholder="Category ..."
+                        value={todoCategory}
+                        onChange={(e) => setTodoCategory(e.target.value)}
+                    />
+                    <Button onClick={handleAdd}>Add Todo</Button>
+                </div>
+
+                {/* SEARCH & FILTER */}
+                <div className="flex gap-2">
+                    <input
+                        className="border p-2 flex-1"
+                        type="text"
+                        placeholder="Search ..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <select
+                        className="border p-2"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    >
+                        <option value="all">All</option>
+                        <option value="work">Work</option>
+                        <option value="personal">Personal</option>
+                    </select>
+                    <select
+                        className="border p-2"
+                        value={sort}
+                        onChange={(e) => setSort(e.target.value as "asc" | "desc")}
+                    >
+                        <option value="desc">Newest</option>
+                        <option value="asc">Oldes</option>
+                    </select>
+                </div>
+
+                {/* LIST */}
+                <div className="space-y-3 py-5">
+                    {todos?.map((todo: Todo) => (
+                        <TodoCard
+                            key={todo.id}
+                            todo={todo}
+                            onToggle={() => updateTodo(
+                                todo.id,
+                                { isCompleted: !todo.isCompleted }
+                            )}
+                            onDelete={() => deleteTodo(todo.id)}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
+
     )
 }
